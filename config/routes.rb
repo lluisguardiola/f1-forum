49d6403a921fd874c8d064b2975ebf1d4f597316
Lluis Guardiola <lluisguardiola.h@gmail.com>
2020-01-03 03:48:08 -0600

    created models, migration, controller and views for User and Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/show'
+  get 'reviews/new'
+  get 'reviews/edit'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/edit'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static#welcomepage'
